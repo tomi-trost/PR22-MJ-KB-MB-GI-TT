bb55239494dcfdee5ad02f1c0d4a05cfa530b24a
irtgal <gal.irt.01@gmail.com>
2023-04-16 17:07:24 +0200

    normaliziraj_imena_stolpcev

--- helper.py
+++ helper.py
@@ -1,21 +1,18 @@
 import pandas as pd
 
 
-# make a function that will take all the column names ["OBČINE", "Občina", "OB�INE", "Kohezijska / Statistična regija", "Kohezijska / statistična regija", "Kohezijska / \nstatistična regija", "Urad za delo"]
+# make a function that will take all the column names 
 # and turn those into a new column name "regija"
 
-REGIJA = {"občine", "občina", "ob�ine", "urad za delo"}
+REGIJA = {"občine", "občina", "ob�ine", "kohezijska / statistična regija", "kohezijska / statistična regija", "kohezijska / \nstatistična regija", "urad za delo"}
 
 LETO = {"leto", "year"}
 
 def normaliziraj_imena_stolpcev(df):
     # find all the columns that have the names in REGIJA and create a new column "regija"
     for imeStolpca in df.columns:
-        print("Col name: ", imeStolpca);
         imeStolpcaLower = imeStolpca.lower().strip()
-        print("Col name: ", imeStolpca);
-        print("Col name: ", imeStolpcaLower);
-        if imeStolpca in REGIJA:
+        if imeStolpcaLower in REGIJA:
             df["__region"] = df[imeStolpca]
             break
         
